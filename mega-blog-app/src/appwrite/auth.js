900344dc39b47b7fe2ef2bee3c9d9a29ca17fc09
Salman <salmanshaikh18786@gmail.com>
2023-12-13 10:54:40 +0530

    appwrite

---
+++ mega-blog-app/src/appwrite/auth.js
@@ -0,0 +1,40 @@
+import config from "../config/config";
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndPoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // call another method
+                return this.login({email, password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw new Error('Error while createAccount: ', error)
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw new Error(`Error while login: ${error}`);
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default AuthService;
